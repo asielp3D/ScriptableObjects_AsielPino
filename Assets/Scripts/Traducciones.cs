8081918b81d5e229268cf49484de19b86473530a
asielp3D <asielmasespacio@gmail.com>
2024-04-10 17:11:38 +0200

    a

---
+++ Assets/Scripts/Traducciones.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Texto", menuName = "ScriptableObjects/Traducciones")]
+public class Traducciones : ScriptableObject
+
+{
+    public string texto1;
+    public string texto2;
+    public string texto3;
+    public string texto4;
+}
+
